d8afb3cb242a275791792f105110e910bd89f8b5
F1ner <alibasis56@gmail.com>
2023-02-27 10:22:22 +0200

    add z1
    компьютерный практикум 4(решили задачу 1)

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            int[] array = new int[15];
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-50, 50);
+                Console.WriteLine($"array[{i}]= {array[i]}"};
+            }
+
+
+        }
+    }
+}
