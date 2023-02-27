a203baf5b0faef4eb16e09e5bc2d898ade88e3dc
F1ner <alibasis56@gmail.com>
2023-02-27 11:04:45 +0200

    delete z2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -29,23 +29,7 @@ namespace ConsoleApp1
             //Console.WriteLine($"Колличество элементов массива кратных 3,={count}");
             
             //            Задание 2
-            int[] array = new int[15];
-            Random random = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = random.Next(-50, 50);
-                Console.WriteLine($"array[{i}]= {array[i]}");
-            }
-            byte count = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] % 3 == 0)
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine($"Колличество элементов массива кратных 3,={count}");
-                Console.Read();
+           
         }
     }
 }
